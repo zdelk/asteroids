f917341a66c594094686dfed38342a2082ac4dbd
zdelk <zmdelk@gmail.com>
2025-03-03 14:52:51 -0500

    Added Triangle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     # Initilizing the game
@@ -9,14 +10,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
     game_clock = pygame.time.Clock()
-    
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     # Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+           
+        
         pygame.Surface.fill(screen, (0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
     
