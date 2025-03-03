38efe6eda1ef2e99334daacbb6aa2918017a0ecb
zdelk <zmdelk@gmail.com>
2025-03-03 14:26:07 -0500

    update

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
         pygame.Surface.fill(screen, (0,0,0))
-        
         pygame.display.flip()
     
     
