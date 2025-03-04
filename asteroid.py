6dec4c2e30f8e8cdf4aa3d83b8bf1a8a6b600c53
zdelk <zmdelk@gmail.com>
2025-03-04 14:22:38 -0500

    Finished base project

--- asteroid.py
+++ asteroid.py
@@ -18,14 +18,12 @@ class Asteroid(CircleShape):
         if self.radius <= ASTEROID_MIN_RADIUS:
             return
         random_angle = random.uniform(20,50)
-        positive_angle = self.rotate(random_angle)
-        negative_angle = self.rotate(-random_angle)
         new_radius = self.radius - ASTEROID_MIN_RADIUS
         
         pos_asteroid = Asteroid(self.position[0], self.position[1], new_radius)
         
         neg_asteroid = Asteroid(self.position[0], self.position[1], new_radius)
         
-        pos_asteroid.velocity = self.velocity.rotate(positive_angle) * 1.2
-        neg_asteroid.velocity = self.velocity.rotate(negative_angle) * 1.2
+        pos_asteroid.velocity = self.velocity.rotate(random_angle) * 1.2
+        neg_asteroid.velocity = self.velocity.rotate(-random_angle) * 1.2
         
